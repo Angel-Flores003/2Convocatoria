5935d4095093ae3f9e98b9114021ab4d26e7ae8a
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-03 16:46:26 +0200

    Complete 14

--- Program.cs
+++ Program.cs
@@ -4,26 +4,30 @@
     {
         public static void Main()
         {
-            int numero;
+            int suma;
+            int[] numeros = new int[5];
 
             try
             {
-                Console.WriteLine("Introdueïx un número per veure la taula de multilicació");
-                numero = int.Parse(Console.ReadLine());
+                Console.WriteLine("Introdueïx 5 números");
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write($"Número {i + 1}: ");
+                    numeros[i] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("\n");
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"El número {numeros[i]} es troba a la possició {i + 1}");
+                }
 
-                TableMultiplicar(numero);
+                suma = numeros.Sum();
+                Console.WriteLine($"\nLa suma dels números és: {suma}");
             }
             catch (FormatException)
             {
                 Console.WriteLine("El valor introduït no és un número vàlid.");
                 return;
-            }
-        }
-        public static void TableMultiplicar(int numero)
-        {
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine($"{numero} x {i} = {numero * i}");
             }
         }
     }
