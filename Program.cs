3b786a8725c99d5c2d62fe1e3352fb685ebb026e
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-03 20:37:07 +0200

    Complete 24

--- Program.cs
+++ Program.cs
@@ -15,9 +15,9 @@
     }
     public class Utilities
     {
-        public static bool IsEven(int num)
+        public static bool IsOdd(int num)
         {
-            return num % 2 == 0;
+            return num % 2 != 0;
         }
     }
 }
