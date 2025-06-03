401564c150bda0bb3b653441f329ad1404c0a5e3
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-03 20:23:17 +0200

    Complete 23

--- Program.cs
+++ Program.cs
@@ -4,36 +4,20 @@
     {
         public static void Main()
         {
-            Random random = new Random();
-            int numero = random.Next(1, 51), input, intentes = 0;
-
+            
             try
-            {
-                Console.WriteLine("Endevina el número secret entre 1 i 50");
-                input = Convert.ToInt32(Console.ReadLine());
-                intentes++;
-
-                while (input != numero)
-                {
-                    if (input < numero)
-                    {
-                        Console.WriteLine("El número secret és més gran. Torna-ho a intentar.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("El número secret és més petit. Torna-ho a intentar.");
-                    }
-                    Console.WriteLine("Torna a provar");
-                    input = Convert.ToInt32(Console.ReadLine());
-                    intentes++;
-                }
-
-                Console.WriteLine($"Enhorabona! Has endevinat el número secret '{numero}' en {intentes} intents");
+            {               
             }
             catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
+            {               
             }
+        }
+    }
+    public class Utilities
+    {
+        public static bool IsEven(int num)
+        {
+            return num % 2 == 0;
         }
     }
 }
