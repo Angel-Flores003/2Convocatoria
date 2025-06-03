dc404d79c46147c044b4ebea5ee806fc29d9dc9e
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-03 19:35:37 +0200

    Complete 22

--- Program.cs
+++ Program.cs
@@ -4,46 +4,36 @@
     {
         public static void Main()
         {
-            int opcio;
-            double temperature = 0;
+            Random random = new Random();
+            int numero = random.Next(1, 51), input, intentes = 0;
 
             try
             {
-                Console.WriteLine("Introdueïx la temperatura en graus Celsius");
-                temperature = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Endevina el número secret entre 1 i 50");
+                input = Convert.ToInt32(Console.ReadLine());
+                intentes++;
 
-                Console.WriteLine("\n[1] Convertir a Fahrenheit\n" +
-                    "[2] Convertir a Kelvin");
-                opcio = Convert.ToInt32(Console.ReadLine());
-
-                switch
-                    (opcio)
+                while (input != numero)
                 {
-                    case 1:
-                        temperature = ConvertToFahrenheit(temperature);
-                        Console.WriteLine("Temperatura en Fahrenheit: " + temperature);
-                        break;
-                    case 2:
-                        temperature = ConvertToKelvin(temperature);
-                        Console.WriteLine("Temperatura en Kelvin: " + temperature);
-                        break;
-                    default:
-                        Console.WriteLine("Opció no valida");
-                        break;
+                    if (input < numero)
+                    {
+                        Console.WriteLine("El número secret és més gran. Torna-ho a intentar.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número secret és més petit. Torna-ho a intentar.");
+                    }
+                    Console.WriteLine("Torna a provar");
+                    input = Convert.ToInt32(Console.ReadLine());
+                    intentes++;
                 }
+
+                Console.WriteLine($"Enhorabona! Has endevinat el número secret '{numero}' en {intentes} intents");
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
-        }
-        public static double ConvertToFahrenheit(double celsius)
-        {
-            return (celsius * 9 / 5) + 32;
-        }
-        public static double ConvertToKelvin(double celsius)
-        {
-            return celsius + 273.15;
         }
     }
 }
